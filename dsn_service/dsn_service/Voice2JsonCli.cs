6c01dba578812f4b4686468b61914520c49c065d
cbaoth <dev@cbaoth.de>
2022-12-16 19:10:35 +0100

    Update Voice2JsonCli re-introduced docker setup, add config parameter

--- dsn_service/dsn_service/Voice2JsonCli.cs
+++ dsn_service/dsn_service/Voice2JsonCli.cs
@@ -56,7 +56,7 @@ namespace DSN
         }
 
         private int runCommand(string command, string args = "", int waitMs = 0, bool elevating = false) {
-             lock (ioLock) {
+            lock (ioLock) {
                 process = new Process();
                 process.StartInfo.FileName = command;
                 process.StartInfo.Arguments = args;
@@ -72,17 +72,16 @@ namespace DSN
                     process.StartInfo.StandardErrorEncoding = System.Text.Encoding.UTF8;
                 }
 
-                Trace.TraceInformation("Executing: {0} {1}", command, args);
+                Trace.TraceInformation("Executing:\n> {0} {1}", command, args);
                 bool ok = false;
                 try {
                     ok = process.Start();
                 } catch(Exception e) {
-                    Trace.TraceError(e.Message);
+                    Trace.TraceError(e.Message); // log exception and continue
                 }
                 if (!ok) {
                     endSession();
-                    throw new Exception(String.Format("Voice2Json execution failed, please make sure that wsl (bash) is available and voice2json is installed correctly: {0} {1}",
-                        command, args));
+                    throw new Exception(String.Format(GetExecutionException(), command, args));
                 }
 
                 if (!elevating) {
@@ -105,7 +104,18 @@ namespace DSN
             }
         }
 
-        private void init()
+        private void init() {
+            if (UseDocke()) {
+                initDocker();
+            } else {
+                initBash();
+            }
+        }
+
+        /**
+         * Init voice2json via local bash (wsl)
+         */
+        private void initBash()
         {
             Trace.TraceInformation("Automatically downloading speech recognition model files");
             runCommand("bash",
@@ -120,9 +130,72 @@ namespace DSN
                 30000); // sec timeout
         }
 
+        /**
+         * Init voice2json via docker
+         */
+        private void initDocker() {
+            int exitCode;
+            if (runCommand("docker", "ps -a") != 0) {
+                string dockerDesktopPath = @"C:\Program Files\Docker\Docker";
+                try {
+                    dockerDesktopPath = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Docker Inc.\Docker\1.0",
+                        "AppPath", @"C:\Program Files\Docker\Docker").ToString();
+                } catch {
+                    // ignore
+                }
+                dockerDesktopPath += @"\Docker Desktop.exe";
+
+                Trace.TraceInformation("Launch Docker Desktop");
+                exitCode = runCommand(dockerDesktopPath, "", 5000, true);
+                if (exitCode == 0) {
+                    Trace.TraceInformation("Wait for Docker Desktop to be ready");
+                    for (int i = 1; i < 20; i++) {
+                        if (runCommand("docker", "ps -a") == 0) {
+                            break;
+                        } else {
+                            Thread.Sleep(5000);
+                        }
+                    }
+                } else if (exitCode != 2) {
+                    Trace.TraceError("Failed to launch Docker Desktop, please make sure Docker Desktop is installed on your system.", exitCode);
+                }
+            }
+
+            Trace.TraceInformation("Launch the docker container 'dsn_voice2json'");
+            exitCode = runCommand("docker", "start dsn_voice2json");
+
+            if (exitCode != 0) {
+                Trace.TraceInformation("Create the docker container 'dsn_voice2json'");
+                exitCode = runCommand("docker", "run -dit --name dsn_voice2json --entrypoint /bin/sh synesthesiam/voice2json");
+
+                if (exitCode != 0) {
+                    throw new Exception("The Voice2Json container cannot be created automatically, " +
+                        "try creating it manually with the following command:\n" +
+                        "docker run -dit --name dsn_voice2json --entrypoint /bin/sh synesthesiam/voice2json");
+                }
+            }
+
+            Trace.TraceInformation("Automatically download speech recognition model files");
+            runCommand("docker", "exec dsn_voice2json sh -c \"" +
+                "/usr/lib/voice2json/bin/voice2json -p " + config.GetLocale() + " train-profile; " +
+                "ls /root/.local/share/voice2json | while read d; " +
+                "do " +
+                    "ln -sf /root/sentences.ini /root/.local/share/voice2json/$d/sentences.ini; " +
+                "done" +
+                "\"");
+        }
+
         public void LoadJSGF(string jsgf) {
+            Trace.TraceInformation("Generating sentences.ini file and running training");
+            string command = UseDocke() ? "docker" : "bash";
+            string args = UseDocke()
+                    ? "exec -i dsn_voice2json sh -c \"base64 -id > /root/sentences.ini; " +
+                      "/usr/lib/voice2json/bin/voice2json -p " + config.GetLocale() + " train-profile\""
+                    : "-c \"" +
+                      "base64 -id > ~/.dsn_sentences.ini; " +
+                      "voice2json -p " + config.GetLocale() + " train-profile" +
+                      "\"";
             lock (ioLock) {
-                Trace.TraceInformation("Generating sentences.ini file and running training");
                 process = new Process();
                 process.StartInfo.RedirectStandardInput = true;
                 process.StartInfo.RedirectStandardOutput = true;
@@ -130,18 +203,14 @@ namespace DSN
                 process.StartInfo.UseShellExecute = false;
                 process.StartInfo.StandardOutputEncoding = System.Text.Encoding.UTF8;
                 process.StartInfo.StandardErrorEncoding = System.Text.Encoding.UTF8;
-                process.StartInfo.FileName = "bash";
-                process.StartInfo.Arguments = "-c \""
-                    + "base64 -id > ~/.dsn_sentences.ini; "
-                    + "voice2json -p " + config.GetLocale() + " train-profile"
-                    + "\"";
+                process.StartInfo.FileName = command;
+                process.StartInfo.Arguments = args;
 
-                Trace.TraceInformation("Executing: {0} {1}", process.StartInfo.FileName, process.StartInfo.Arguments);
+                Trace.TraceInformation("Executing: {0} {1}", command, args);
 
                 if (!process.Start()) {
                     endSession();
-                    throw new Exception(String.Format("Voice2Json execution failed, please make sure that bash (wsl) is available and voice2json is installed correctly: {0} {1}",
-                        process.StartInfo.FileName, process.StartInfo.Arguments));
+                    String.Format(String.Format(GetExecutionException() + ":\n> {0} {1}", command, args));
                 }
 
                 readStdOut = new Thread(ReadStdOut);
@@ -158,8 +227,7 @@ namespace DSN
                 endSession();
 
                 if (exitCode != 0) {
-                    throw new Exception(String.Format("Voice2Json execution failed, please make sure that bash (wsl) is available and voice2json is installed correctly: {0} {1}",
-                        process.StartInfo.FileName, process.StartInfo.Arguments));
+                    String.Format(String.Format(GetExecutionException() + ":\n> {0} {1}", command, args));
                 }
             }
         }
@@ -211,6 +279,15 @@ namespace DSN
         }
 
         public void RecognizeAsync() {
+            string command = UseDocke() ? "docker" : "bash";
+            // TODO consolidate
+            string args = UseDocke()
+                    ? "exec -i dsn_voice2json sh -c \"base64 -id | /usr/lib/voice2json/bin/voice2json -p " +
+                      config.GetLocale() + " transcribe-stream --audio-source -" +
+                      " | /usr/lib/voice2json/bin/voice2json -p " + config.GetLocale() + " recognize-intent\""
+                    : "-c \"base64 -id | voice2json -p " +
+                      config.GetLocale() + " transcribe-stream --audio-source -" +
+                      " | voice2json -p " + config.GetLocale() + " recognize-intent\"";
             lock (ioLock) {
                 process = new Process();
                 process.StartInfo.RedirectStandardInput = true;
@@ -219,16 +296,13 @@ namespace DSN
                 process.StartInfo.UseShellExecute = false;
                 process.StartInfo.StandardOutputEncoding = System.Text.Encoding.UTF8;
                 process.StartInfo.StandardErrorEncoding = System.Text.Encoding.UTF8;
-                process.StartInfo.FileName = "bash";
-                process.StartInfo.Arguments = "-c \"base64 -id | voice2json -p "
-                    + config.GetLocale() + " transcribe-stream --audio-source -"
-                    + " | voice2json -p " + config.GetLocale() + " recognize-intent\"";
+                process.StartInfo.FileName = command;
+                process.StartInfo.Arguments = args;
                 Trace.TraceInformation("Executing: {0} {1}", process.StartInfo.FileName, process.StartInfo.Arguments);
 
                 if (!process.Start()) {
                     endSession();
-                    throw new Exception(String.Format("Voice2Json execution failed, please make sure that bash (wsl) is available and voice2json is installed correctly: {0} {1}",
-                        process.StartInfo.FileName, process.StartInfo.Arguments));
+                    throw new Exception(String.Format(GetExecutionException() + ":\n> {0} {1}", command, args));
                 }
 
                 stdIn = process.StandardInput;
@@ -238,6 +312,7 @@ namespace DSN
                 readStdErr.Start();
             }
         }
+
         private void WaveSourceDataAvailable(object sender, WaveInEventArgs e) {
             //Trace.TraceInformation("WaveSourceDataAvailable: {0}, {1}", sessionId, e.BytesRecorded);
             lock (ioLock) {
@@ -249,6 +324,15 @@ namespace DSN
 
         private string Base64Encode(byte[] buffer) {
             return System.Convert.ToBase64String(buffer);
+        }
+
+        private string GetExecutionException() {
+            return UseDocke() ? "Run docker commnad failed, make sure you have Docker Desktop installed"
+                    : "Voice2Json execution failed, please make sure that bash (wsl) is available and voice2json is installed correctly";
+        }
+
+        private bool UseDocke() {
+            return config.Get("SpeechRecognition", "bVoice2JsonUseDocker", "1") == "1";
         }
     }
 }
